2b884d4fb3af183812883a5c434b7f8e80aebebb
Jamroz <bjamr@wro-bjamr-mbp.dolby.net>
2025-05-23 16:05:12 +0200

    Poprawki w monitoring_bot.py

--- monitoring_bot.py
+++ monitoring_bot.py
@@ -6,11 +6,11 @@ from threading import Thread
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 # Pobieranie konfiguracji ze zmiennych środowiskowych
-URLS_TO_CHECK = os.getenv("URLS_TO_CHECK", "")
+URLS_TO_CHECK = "https://dolby.okta.com/,https://confluence.dolby.net/kb/,https://app.smartsheet.com/,https://dolby.ent.box.com/"
 URLS_TO_CHECK = [url.strip() for url in URLS_TO_CHECK.split(",") if url.strip()]
 
 CHECK_INTERVAL_SECONDS = int(os.getenv("CHECK_INTERVAL_SECONDS", "300"))
-TEAMS_WEBHOOK_URL = os.getenv("TEAMS_WEBHOOK_URL")
+TEAMS_WEBHOOK_URL = "https://dolby.webhook.office.com/webhookb2/da4607bf-4106-4b9d-88a1-0e8087794137@05408d25-cd0d-40c8-8962-5462de64a318/IncomingWebhook/f3fbd221012c4b88987a792b5bb53d52/cc9a05af-8a0a-46a2-8400-6f60cedfeded/V2Ewmi8EB0jwyrES6crn4lD417vrYZUZzHX1R9tmRenP81"
 
 if not TEAMS_WEBHOOK_URL:
     print("Błąd: brak TEAMS_WEBHOOK_URL w zmiennych środowiskowych!")
